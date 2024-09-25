5da06f13fbc11e8b7bb44b0c5550d0edd714f12b
whitgroves <whitney.groves@gmail.com>
2024-09-25 08:44:18 -0500

    backport crystal learnsets pt 2 (double team)

--- maps/CeladonGameCornerPrizeRoom.asm
+++ maps/CeladonGameCornerPrizeRoom.asm
@@ -41,10 +41,10 @@ CeladonPrizeRoom_tmcounterloop:
 .DoubleTeam:
 	checkcoins CELADONGAMECORNERPRIZEROOM_TM32_COINS
 	ifequal HAVE_LESS, CeladonPrizeRoom_notenoughcoins
-	getitemname STRING_BUFFER_3, TM_DOUBLE_TEAM
+	getitemname STRING_BUFFER_3, TM_FLAMETHROWER
 	scall CeladonPrizeRoom_askbuy
 	iffalse CeladonPrizeRoom_CancelPurchaseScript
-	giveitem TM_DOUBLE_TEAM
+	giveitem TM_FLAMETHROWER
 	iffalse CeladonPrizeRoom_notenoughroom
 	takecoins CELADONGAMECORNERPRIZEROOM_TM32_COINS
 	sjump CeladonPrizeRoom_purchased
