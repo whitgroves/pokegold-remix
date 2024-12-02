425831f26dd86542393398eb8d4eb950504bf8bb
whitgroves <whitney.groves@gmail.com>
2024-12-02 03:14:33 -0600

    tree encounters

--- data/wild/treemons.asm
+++ data/wild/treemons.asm
@@ -18,78 +18,58 @@ TreeMonSet_None:
 TreeMonSet_Unused:
 TreeMonSet_City:
 ; common
-	db 50, VENONAT,    15
-	db 30, VENONAT,    15
-	db 10, ABRA,       15
-	db  5, ABRA,       15
-	db  5, VENOMOTH,   15
+	db 50, HOOTHOOT,   10
+	db 30, SPEAROW,    10
+	db 10, PIDGEY,     10
+	db  5, LEDYBA,     10
+	db  5, SPINARAK,   10
 	db -1
 ; rare
-	db 50, VENONAT,    15
-	db 30, MAGNEMITE,  15
+	db 50, HOOTHOOT,   15
+	db 30, MEOWTH,     15
 	db 10, ABRA,       15
-	db  5, ABRA,       15
-	db  5, VENOMOTH,   15
+	db  5, MURKROW,    15
+	db  5, MAGNEMITE,  15
 	db -1
 
 TreeMonSet_Forest:
 
-IF DEF(_GOLD)
 ; common
-	db 50, CATERPIE,   10
-	db 15, CATERPIE,   10
-	db 15, METAPOD,    10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
+	db 50, PINECO,     10
+	db 15, AIPOM,      10
+	db 15, MANKEY,     10
+	db 10, PARAS,  	   10
 	db  5, BUTTERFREE, 10
-	db -1
-; rare
-	db 50, CATERPIE,   10
-	db 15, PINECO,     10
-	db 15, PINECO,     10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, BUTTERFREE, 10
-	db -1
-
-ELIF DEF(_SILVER)
-; common
-	db 50, WEEDLE,     10
-	db 15, WEEDLE,     10
-	db 15, KAKUNA,     10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
 	db  5, BEEDRILL,   10
 	db -1
 ; rare
-	db 50, WEEDLE,     10
-	db 15, PINECO,     10
-	db 15, PINECO,     10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, BEEDRILL,   10
+	db 50, HOOTHOOT,   15
+	db 15, TANGELA,    15
+	db 15, SCYTHER,    15
+	db 10, HERACROSS,  15
+	db  5, PINSIR,     15
+	db  5, PIKACHU,    15
 	db -1
-ENDC
 
 TreeMonSet_Canyon:
 ; common
-	db 50, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, HOOTHOOT,   10
+	db 15, LEDYBA,     10
+	db 15, SPINARAK,   10
+	db 10, HOPPIP,     10
+	db  5, SPEAROW,    10
+	db  5, PIDGEY,     10
 	db -1
 ; rare
-	db 50, SPEAROW,    10
-	db 15, HERACROSS,  10
-	db 15, HERACROSS,  10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, HOOTHOOT,   15
+	db 15, EKANS,	   15
+	db 15, FURRET,	   15
+	db 10, JIGGLYPUFF, 15
+	db  5, FARFETCH_D, 15
+	db  5, TEDDIURSA,  15
 	db -1
 
 TreeMonSet_Rock:
-	db 90, KRABBY,     15
-	db 10, SHUCKLE,    15
+	db 80, KRABBY,     20
+	db 20, SHUCKLE,    20
 	db -1
