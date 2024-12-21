45d6fd80b9bb5f2299f1f41a9823656cd3942a66
whitgroves <whitney.groves@gmail.com>
2024-12-21 03:02:56 -0600

    crunch

--- data/pokemon/evos_attacks.asm
+++ data/pokemon/evos_attacks.asm
@@ -1795,6 +1795,7 @@ PinsirEvosAttacks:
 	db 31, GUILLOTINE
 	db 37, SUBMISSION
 	db 43, SWORDS_DANCE
+	db 50, CRUNCH
 	db 0 ; no more level-up moves
 
 TaurosEvosAttacks:
@@ -1828,7 +1829,8 @@ GyaradosEvosAttacks:
 	db 35, TWISTER
 	db 40, HYDRO_PUMP
 	db 45, RAIN_DANCE
-	db 50, HYPER_BEAM
+	db 50, CRUNCH
+	db 55, HYPER_BEAM
 	db 0 ; no more level-up moves
 
 LaprasEvosAttacks:
@@ -1986,14 +1988,17 @@ KabutopsEvosAttacks:
 
 AerodactylEvosAttacks:
 	db 0 ; no more evolutions
+	db 1, SWIFT
 	db 1, WING_ATTACK
-	db 8, AGILITY
+	db 8, SCARY_FACE
 	db 15, BITE
-	db 22, SUPERSONIC
+	db 22, AGILITY
 	db 29, ANCIENTPOWER
-	db 36, SCARY_FACE
-	db 43, TAKE_DOWN
-	db 50, HYPER_BEAM
+	db 36, DRAGONBREATH
+	db 43, CRUNCH
+	db 50, SCREECH
+	db 57, FLY
+	db 64, HYPER_BEAM
 	db 0 ; no more level-up moves
 
 SnorlaxEvosAttacks:
@@ -3045,15 +3050,13 @@ TeddiursaEvosAttacks:
 	db 29, REST
 	db 36, SLASH
 	db 43, SNORE
-	db 50, THRASH
+	db 50, TAKE_DOWN
 	db 0 ; no more level-up moves
 
 UrsaringEvosAttacks:
 	db 0 ; no more evolutions
 	db 1, SCRATCH
 	db 1, LEER
-	db 1, LICK
-	db 1, FURY_SWIPES
 	db 8, LICK
 	db 15, FURY_SWIPES
 	db 22, FAINT_ATTACK
@@ -3061,7 +3064,7 @@ UrsaringEvosAttacks:
 	db 29, REST
 	db 39, SLASH
 	db 49, SNORE
-	db 59, THRASH
+	db 59, TAKE_DOWN
 	db 0 ; no more level-up moves
 
 SlugmaEvosAttacks:
