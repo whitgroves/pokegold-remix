7f151178074706516af352898023f94e64822f3b
whitgroves <whitney.groves@gmail.com>
2025-05-12 14:44:27 -0500

    buff mahogany gym

--- data/trainers/attributes.asm
+++ data/trainers/attributes.asm
@@ -197,7 +197,7 @@ TrainerClassAttributes:
 ; Skier
 	db NO_ITEM, NO_ITEM ; items
 	db 18 ; base reward
-	dw AI_BASIC | AI_SETUP | AI_TYPES | AI_STATUS | AI_RISKY
+	dw AI_BASIC | AI_TYPES | AI_OPPORTUNIST | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
 ; Teacher
@@ -347,7 +347,7 @@ TrainerClassAttributes:
 ; Boarder
 	db NO_ITEM, NO_ITEM ; items
 	db 18 ; base reward
-	dw AI_BASIC | AI_TYPES | AI_OPPORTUNIST | AI_STATUS
+	dw AI_BASIC | AI_TYPES | AI_SMART | AI_STATUS
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
 ; Pokefanm
